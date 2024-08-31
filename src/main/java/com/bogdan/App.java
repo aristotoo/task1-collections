16b9065b474564ed9b4c70fb0d239066a8880d72
Bogdan Rotozey <bogdan.rotozej@gmail.com>
2024-08-31 17:40:54 +0300

    Initial commit

---
+++ src/main/java/com/bogdan/App.java
@@ -0,0 +1,12 @@
+package com.bogdan;
+
+/**
+ * Класс (@code App) точка входа приложения
+ *
+ */
+public class App {
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
